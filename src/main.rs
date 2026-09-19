36a5dca0434c025ced2f5a214494d16fb0058f4c
Nate River <river177169@proton.me>
2026-09-19 09:44:57 +0530

    init

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,15 @@
 
+mod data;
+
+
+
+static TEST_CSV: &[u8]=include_bytes!("../data/test.csv");
+static TRAIN_CSV: &[u8]=include_bytes!("../data/train.csv");
+static TRAIN_LABELS_CSV: &[u8]=include_bytes!("../data/train_labels.csv");
+
+pub struct AppState {
+}
+
 
 
 fn main()-> anyhow::Result<()> {
@@ -8,6 +19,29 @@ fn main()-> anyhow::Result<()> {
 
 
   Ok(())
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+impl AppState {
+  pub fn init()-> anyhow::Result<Self> {
+
+    todo!()
+  }
 }
 
 
